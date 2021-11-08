acbb4f97c2875a27e9b6f84ca1ba60b8acade5fe
Greg Brimble <gbrimble@cloudflare.com>
2021-11-08 15:03:27 +0000

    Branding

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,20 @@
 import type { FC } from "react";
+import { Link } from "react-router-dom";
 
 export const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="admin">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        <h1>Image sharing platform built on Cloudflare Pages</h1>
+      </header>
+      <main></main>
+    </div>
+  );
 };
